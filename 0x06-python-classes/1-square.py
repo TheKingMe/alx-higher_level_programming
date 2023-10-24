0abb98076dde09f055593845dc368ac7ddea8d70
Ismail <im179242@gmail.com>
2023-10-24 23:28:59 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""class Square"""
+
+
+class Square:
+    """this a square"""
+
+    def __init__(self, size):
+        """new Square
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
+        
